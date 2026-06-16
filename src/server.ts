afae56cb2f3685698eae69166e831ca1e7a8244b
Viktor Gabriel <gabryoliver22@gmail.com>
2026-06-16 16:15:20 -0300

    docs: configurado documentação interativa da API utilizando swagger ui

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
 import { taskRoutes } from './routes/task.routes';
 import { errorHandler } from './errors/AppError';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// Documentação da API com Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Rota de teste
 app.get('/', (req: Request, res: Response) => {
@@ -20,4 +25,5 @@ app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`[server]: Servidor ativo na porta http://localhost:${PORT}`);
+    console.log(`[swagger]: Documentação disponível em http://localhost:${PORT}/api-docs`);
 });
